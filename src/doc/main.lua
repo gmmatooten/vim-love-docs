6bf0e5d4602bed6fa54844bd6b639d73a8de3d73
Davis Claiborne <davisclaib@gmail.com>
2017-08-04 17:46:34 -0400

    Begin rewrite of rewrite :)

--- src/doc/main.lua
+++ src/doc/main.lua
@@ -33,7 +33,7 @@ local tabWidth = 4
 
 -- TODO: Handle using actual tabs (\t) in text
 -- (width calculation currently relies on the length of string, so spaces are required)
-local tabStr = (' '):rep( tabWidth )
+local tabString = (' '):rep( tabWidth )
 
 -- Width of tags in the table of contents
 local tableOfContentsTagWidthLimit = 20
@@ -41,7 +41,7 @@ local tableOfContentsTagWidthLimit = 20
 -- Give align module these changes
 align.setDefaultWidth( lineWidth )
 align.setTabWidth( tabWidth )
-align.setTabStr( tabStr )
+align.setTabStr( tabString )
 -- }}}
 
 -- Header {{{
@@ -62,12 +62,8 @@ local header = [[
 
 -- Misc. utility functions {{{
 -- Shorten str if it is longer than width
-local function abbreviateString( str, width )
+local function shortenString( str, width )
 	return #str < width and str or str:sub( 1, width - 1 ) .. '-'
-end
-
-local function basicDescription( aspect, funcName )
-	return 'The ' .. aspect .. ' for ' .. funcName
 end
 -- }}}
 
@@ -107,185 +103,100 @@ end
 -- }}}
 -- }}}
 
--- Sections {{{
--- Keep track of all the tags used to find any matching text
-local tags = {}
+-- Gets the aspects of module[attribute] as a string
+-- module is the table containing the module's information
+-- attributeName is the name of the aspect you would like to get, e.g. types, enums, etc.
+-- tagPrefix is the prefix that precedes all tags
+-- parentName is the name of the attribute's parent (i.e. love.audio, etc.)
+local function getAttributeInformation( module, attributeName, tagPrefix, parentName )
+	tagPrefix = tagPrefix or ''
 
--- Stores the contents of the table of contents
-local tableOfContents = ''
+	-- Right align the tag
+	local returnString = align.right( formatStringAsTag( module.name .. '-' .. attributeName ) ) .. '\n'
+	-- Include the reference number and name
+	.. referenceNumber .. ' ' .. attributeName .. ':\n\n'
+	-- And a (very) basic description
+	.. 'The ' .. attributeName .. ' of ' .. parentName .. '.' .. '\n\n'
 
--- Stores the contents of as a table (can't use a string; not enough memory)
-local contents = {}
+	-- Show the contents of the attributeName (if any)
+	if module[attributeName] and #module[attributeName] > 0 then
+		for _, attr in ipairs( module[attributeName] ) do
+			local line = tabString .. attr.name
+			line = line .. align.right( formatStringAsRef( tagPrefix .. attr.name ), ' ', lineWidth - #line )
 
--- referenceNumber and name are used for the listing
--- tag is the string used as the ref/tag (NOT '|love-tag|', just 'tag')
-local function addSection( referenceNumber, module, tagName, tag, aspectName, funcName )
-	tag = tag or tagName
-
-	-- Use a hash to make looking up words easier, plus prevents duplicates
-	tags[tag] = true
-
-	if type( module ) == 'table' then
-		module.description = module.description or basicDescription( aspectName, funcName )
-	end
-
-	-- Table of contents {{{
-	-- Determine indentation level
-	local _, numberOfIndents = referenceNumber:gsub( '%.', '' )
-	numberOfIndents = numberOfIndents - 1
-	local indentation = tabStr:rep( numberOfIndents )
-
-	-- Format tag to the specified length
-	local tableOfContentsTag = abbreviateString( tag, tableOfContentsTagWidthLimit )
-
-	-- Space before is for padding
-	tableOfContentsTag = ' ' .. formatStringAsRef( tableOfContentsTag )
-
-	-- Shorten listing if it's too long
-	local listingNumberAndName = referenceNumber .. ' ' .. tagName
-	local tableOfContentsListing = indentation .. listingNumberAndName
-	-- (-2 is for space and period for separation)
-	local widthLimit = lineWidth - #tableOfContentsTag - 2
-	tableOfContentsListing = abbreviateString( tableOfContentsListing, widthLimit )
-
-	-- Space after is for padding
-	tableOfContentsListing = tableOfContentsListing .. ' '
-
-	-- Right-align tag
-	-- (width includes #tableOfContentsListing to account for text already on the line)
-	local rightAlignedTag = align.right( tableOfContentsTag, '.', lineWidth - #tableOfContentsListing )
-
-	tableOfContentsListing = tableOfContentsListing .. rightAlignedTag
-	tableOfContents = tableOfContents .. '\n' .. tableOfContentsListing
-	-- }}}
-
-	-- Contents {{{
-	local currentContent = ''
-	if numberOfIndents == 0 then
-		currentContent = currentContent .. ('='):rep( lineWidth )
+			returnString = returnString .. line .. '\n'
+		end
 	else
-		currentContent = currentContent .. ('-'):rep( lineWidth )
+		returnString = returnString .. tabString .. 'None\n'
 	end
 
-	currentContent = currentContent .. '\n' .. align.right( abbreviateString(
-		formatStringAsTag( tag ), lineWidth ), ' '
-	) .. '\n' .. listingNumberAndName .. '\n\n'
-
-	-- Description
-	.. align.left( module.description or '', 0 ) .. '\n'
-	-- Types
-	.. ( module.types and #module.types > 0 and '\nTypes:\n' or '' )
-	-- Supertypes
-	-- Subtypes
-	-- ParentType
-	-- Constructors
-	-- Enums
-	-- Constants
-	-- Callbacks
-	-- Functions
-
-	-- Variants
-	-- Notes
-
-	-- Add currentContents to content
-	table.insert( contents, currentContent )
-	-- }}}
+	return returnString
 end
--- }}}
 
--- Extract data and subdata {{{
--- Declare extractData earlier, so that extractSubData can reference it
-local extractData
+-- Gets the overview of the module
+-- module is the table containing the module's information
+local function getModuleOverview( module )
+	local returnString = align.right( formatStringAsTag( module.name ) ) .. '\n'
+	.. referenceNumber .. ' ' .. module.name .. '\n'
+	.. '\n'
+	.. align.left( module.description ) .. '\n'
 
--- Extract specific sub-elements of a module (sectionName), such as types/enums
--- module, prefix, and funcSeparator all have the same meaning as with extractData
-local function extractSubData( module, sectionName, prefix, funcSeparator, funcName )
-	local section = module[sectionName]
+	return returnString
+end
 
-	if section and #section > 0 then
-		-- Update reference number and add new subsection
-		incrementRefNumber()
-		addSection( referenceNumber, section, sectionName, module.name .. '-' .. sectionName, sectionName, funcName )
+for _, module in ipairs( api.modules ) do
+	incrementRefNumber()
+
+	local parentName = ('love.%s'):format( module.name )
+
+	print( getModuleOverview( module ) )
+
+	addRefNumberSection()
+
+	incrementRefNumber()
+	print( getAttributeInformation( module, 'types', '', parentName ) )
+
+	if module.types then
 		addRefNumberSection()
 
-		-- Loop over information in section
-		for _, moduleSubData in ipairs( section ) do
-			extractData( moduleSubData, prefix, funcSeparator )
+		for _, Type in ipairs( module.types ) do
+			incrementRefNumber()
+			print( getModuleOverview( Type ) )
+
+			addRefNumberSection()
+			incrementRefNumber()
+			print( getAttributeInformation( Type, 'functions', Type.name .. ':', Type.name ) )
+			removeRefNumberSection()
+		end
+
+		-- constructors
+		-- parenttype
+		-- supertype
+
+		removeRefNumberSection()
+	end
+
+	incrementRefNumber()
+	print( getAttributeInformation( module, 'enums', '', parentName ) )
+
+	if module.enums then
+		addRefNumberSection()
+
+		for _, enum in ipairs( module.enums ) do
+			incrementRefNumber()
+			print( getModuleOverview( enum ) )
+
+			addRefNumberSection()
+			incrementRefNumber()
+			print( getAttributeInformation( enum, 'constants', enum.name .. '-', enum.name ) )
+			removeRefNumberSection()
 		end
 
 		removeRefNumberSection()
 	end
-end
 
--- Loop over modules
--- module is the table containing the api data
--- prefix is the name that prefaces tags (not including modulePrefix)
--- funcSeparator specifies how to separate functions ('.', ':', etc.)
-function extractData( module, prefix, funcSeparator )
-	-- Give parameters defaults
-	prefix = prefix or 'love.'
-	funcSeparator = funcSeparator or '.'
-
-	local funcName = prefix .. module.name
-
-	-- Increment reference and add section about the current module
 	incrementRefNumber()
-	addSection( referenceNumber, module, module.name, module.name, funcName )
-
-	-- Extract data from module {{{
-	-- Make a new section for sub-information (types, etc.)
-	addRefNumberSection()
-
-	-- Types
-	extractSubData( module, 'types', '', ':', funcName )
-
-	-- Supertypes
-	if module.supertypes and #module.supertypes > 0 then
-		incrementRefNumber()
-		addSection( referenceNumber, module.supertypes, module.name .. '-supertypes', module.name .. '-supertypes', 'supertypes', funcName )
-	end
-
-	-- Subtypes
-	if module.subtypes and #module.subtypes > 0 then
-		incrementRefNumber()
-		addSection( referenceNumber, module.subtypes, module.name .. '-subtypes', module.name .. '-subtypes', 'subtypes', funcName )
-	end
-
-	-- ParentType
-	if module.parenttype then
-		incrementRefNumber()
-		addSection( referenceNumber, module.parenttype, module.name .. '-parenttype', module.name .. '-parenttype', 'parenttype', funcName )
-	end
-
-	-- Constructors
-	if module.constructors and #module.constructors > 0 then
-		incrementRefNumber()
-		addSection( referenceNumber, module.constructors, module.name .. '-constructors', module.name .. '-constructors', 'constructors', funcName )
-	end
-
-	-- Enums
-	extractSubData( module, 'enums', '', ':', funcName )
-
-	-- Constants
-	extractSubData( module, 'constants', module.name .. '-', '-', funcName )
-
-	-- Callbacks
-	extractSubData( module, 'callbacks', funcName .. funcSeparator, funcSeparator, funcName )
-
-	-- Functions
-	extractSubData( module, 'functions', funcName .. funcSeparator, funcSeparator, funcName )
+	print( getAttributeInformation( module, 'functions', parentName .. '.', parentName  ) )
 
 	removeRefNumberSection()
-	-- }}}
 end
--- }}}
-
--- Output the documentation {{{
-for _, module in ipairs( api.modules ) do
-	extractData( module )
-end
-
-print( header )
-print( tableOfContents .. '\n' )
-print( table.concat( contents, '\n' ) )
--- }}}
